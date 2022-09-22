57fb3f3b159c1d413696a6ebd95a287894e65c9d
mulztob <mulztob@gmail.com>
2022-09-22 14:50:03 +0200

    resolve species from state/backend however necessary

--- src/components/people-card/people-card.tsx
+++ src/components/people-card/people-card.tsx
@@ -1,6 +1,8 @@
 import { Component, h, Prop } from '@stencil/core';
 // import state from '../../films-store';
-import { IPeople } from 'swapi-ts';
+import { IPeople, ISpecie } from 'swapi-ts';
+import * as SWApi from 'swapi-ts';
+import state from '../../store';
 
 @Component({
   tag: 'people-card',
@@ -9,6 +11,37 @@ import { IPeople } from 'swapi-ts';
 })
 export class PeopleCard {
   @Prop() person: IPeople;
+  resolvedSpecies: ISpecie[] = [];
+
+  async componentWillLoad() {
+    this.resolvedSpecies = [];
+    if (this.person?.species?.length > 0) {
+      const species = this.person.species;
+
+      if (typeof species.at(0) === 'string') {
+        for await (const maybeResolved of species) {
+          const fromState = this.loadFromState(maybeResolved as string);
+
+          this.needsSwapiResolve(fromState)
+            ? (this.resolvedSpecies = this.resolvedSpecies.concat(await this.updateState(maybeResolved)))
+            : (this.resolvedSpecies = this.resolvedSpecies.concat(fromState as ISpecie));
+        }
+      }
+    }
+  }
+
+  private async updateState(maybeResolved: string | ISpecie) {
+    return (state.species[maybeResolved as string] = (await SWApi.Species.find(q => maybeResolved === q.url)).resources.at(0).value);
+  }
+
+  private loadFromState(url: string): ISpecie | string {
+    if (url == null) return;
+    return state.species[url] ?? url;
+  }
+
+  private needsSwapiResolve(species: string | ISpecie) {
+    return typeof species === 'string';
+  }
 
   render() {
     return (
@@ -16,8 +49,9 @@ export class PeopleCard {
         {/* <img src="img_avatar.png" width="100%" /> */}
         <div class="container">
           <h4>Name: {this.person.name}</h4>
+          {/* <p>Id: {this.person.url}</p> */}
           <p>Gender: {this.person.gender}</p>
-          <p>Species: {this.person.species}</p>
+          <p>Species: {this.resolvedSpecies.map(sp => sp.name).join(', ')}</p>
         </div>
       </div>
     );
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,22 @@
 import { createStore } from '@stencil/store';
-import { IFilm } from 'swapi-ts';
+import { IFilm, ISpecie } from 'swapi-ts';
 
 interface State {
   films: IFilm[];
+  species: Record<string, ISpecie>;
 }
 
 const { state, onChange } = createStore<State>({
   films: [],
+  species: {},
 });
 
 onChange('films', value => {
   state.films = value;
+});
+
+onChange('species', value => {
+  state.species = value;
 });
 
 export default state;
