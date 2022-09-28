2e1a74c64a019a0d4dac381129b22fa9f39ffb92
mulztob <mulztob@gmail.com>
2022-09-28 12:06:13 +0200

    .

--- src/components/app-root/app-root.e2e.ts
+++ src/components/app-root/app-root.e2e.ts
@@ -1,5 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
-
+jest.setTimeout(10000);
 describe('app-root', () => {
   it('renders', async () => {
     const page = await newE2EPage();
@@ -13,7 +13,9 @@ describe('app-root', () => {
     const page = await newE2EPage();
     await page.setContent('<app-root></app-root>');
     await page.waitForChanges();
+
     const element = await page.find('app-root >>> h1');
-    expect(element).toEqualText('Star Wars API Test App using Stencil.JS');
+
+    expect(element.innerText).toEqual('Star Wars API Test App using Stencil.JS');
   });
 });
--- src/components/films-details/films-details.e2e.ts
+++ src/components/films-details/films-details.e2e.ts
@@ -1,31 +1,25 @@
 import { newE2EPage } from '@stencil/core/testing';
-import { resetStore } from '../../store/store.testwrapper';
-// import store from '../../store/store';
-// import { jest } from '@jest/globals';
-
-beforeEach(() => {
-  // resetStore();
-  // console.log('beforeEach, store.state.films: ', store.state.films);
-});
+import { film1FullDetails } from '../../store/store.testwrapper';
 
 async function setupComponent() {
   const page = await newE2EPage();
 
-  // console.log('setupComponent, store.state.films: ', store.state.films);
+  await page.setContent(`<films-details></films-details>`);
 
-  await page.setContent(`<films-details episodeId="'1'"></films-details>`);
-  await page.$eval('films-details', elm => {
-    // within the browser's context
-    // let's set new property values on the component
-    elm.episodeId = '1';
-  });
+  await page.$eval(
+    'films-details',
+    (detailsNode, film) => {
+      detailsNode.episodeId = '1';
+      detailsNode.film = film;
+    },
+    film1FullDetails,
+  );
 
   await page.waitForChanges();
-  // console.log('page#1', page);
   return page;
 }
 
-describe('films-details', () => {
+describe('films-details, component details', () => {
   it('renders', async () => {
     const page = await setupComponent();
 
@@ -33,15 +27,14 @@ describe('films-details', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('displays the specified name "The Phantom Menace"', async () => {
+  it('displays the specified name "The Phantom Menace" as H1', async () => {
     const page = await setupComponent();
-    // console.log('page#2:', page);
-    const filmTitle = await page.find('films-details');
-    console.log('film#1', filmTitle.textContent);
+    const filmTitle = await page.find('films-details >>> h1');
+    // console.log('film#1', filmTitle.textContent);
 
     // const element = filmTitle.shadowRoot.querySelector('div');
     // const element = filmTitle.querySelector('div');
-    expect(filmTitle.innerHTML).toContain('The Phantom Menace');
+    expect(filmTitle.innerText).toContain('The Phantom Menace');
   });
 
   // it('includes a div with the class "films-details"', async () => {
@@ -51,4 +44,22 @@ describe('films-details', () => {
   //   const element = await page.find('app-root >>> films-details >>> div');
   //   expect(element).toHaveClass('films-details');
   // });
+});
+
+describe('films-details, navigation', () => {
+  it('displays a back button', async () => {
+    const page = await setupComponent();
+    const back = await page.find('films-details >>> button');
+    expect(back.innerText).toEqual('back');
+  });
+
+  it('back button should route', async () => {
+    const page = await setupComponent();
+    const back = await page.find('films-details >>> button');
+
+    const oldUrl = page.url();
+    await back.click();
+    await page.waitForChanges();
+    expect(page.url()).not.toEqual(oldUrl);
+  });
 });
